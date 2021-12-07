70cc146efee392f6f2512d8a2cc1d58d2a62bd6d
Armelle Loiseau <armelle.loiseau@gmail.com>
2021-12-07 20:00:01 +0100

    tried my best at the generic function but i have no idea wether it's working or not

--- src/viking.js
+++ src/viking.js
@@ -1,16 +1,113 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+
+  attack() {
+    return this.strength;
+  }
+
+  receiveDamage(damage) {
+    this.health -= damage;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.health > 0)
+      return `${this.name} has received ${damage} points of damage`;
+    else return `${this.name} has died in act of combat`;
+  }
+  battleCry() {
+    return 'Odin Owns You All!';
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.health > 0)
+      return `A Saxon has received ${damage} points of damage`;
+    else return `A Saxon has died in combat`;
+  }
+}
 
 // War
-class War {}
+class War {
+  constructor() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  }
 
+  addViking(vikingObj) {
+    this.vikingArmy.push(vikingObj);
+  }
 
+  addSaxon(saxonObj) {
+    this.saxonArmy.push(saxonObj);
+  }
+
+  vikingAttack() {
+    let randomSaxonIndex = Math.floor(Math.random() * this.saxonArmy.length);
+    let randomSaxon = this.saxonArmy[randomSaxonIndex];
+    let randomVikingIndex = Math.floor(Math.random() * this.vikingArmy.length);
+    let randomViking = this.vikingArmy[randomVikingIndex];
+
+    let casualty = randomSaxon.receiveDamage(randomViking.strength);
+    console.log('I AM A casualty');
+
+    console.log(casualty);
+    if (randomSaxon.health <= 0) {
+      this.saxonArmy.splice(randomSaxonIndex, 1);
+    }
+    return casualty;
+  }
+
+  saxonAttack() {
+    let randomSaxonIndex = Math.floor(Math.random() * this.saxonArmy.length);
+    let randomSaxon = this.saxonArmy[randomSaxonIndex];
+    let randomVikingIndex = Math.floor(Math.random() * this.vikingArmy.length);
+    let randomViking = this.vikingArmy[randomVikingIndex];
+
+    let casualty = randomViking.receiveDamage(randomSaxon.strength);
+
+    if (randomViking.health <= 0) {
+      this.vikingArmy.splice(randomVikingIndex, 1);
+    }
+    return casualty;
+  }
+
+  genericAttack(attacker, target) {
+    // I'm assuming that the two arguments can only be saxonArmy & vikingArmy, either as attacker or target
+    
+    let randomAttackerIndex = Math.floor(Math.random() * attacker.length);
+    let randomAttacker = attacker[randomAttackerIndex];
+    let randomTargetIndex = Math.floor(Math.random() * target.length);
+    let randomTarget = this.targetArmy[randomTargetIndex];
+
+    let casualty = randomTarget.receiveDamage(randomAttacker.strength);
+    if (randomTarget.health <= 0) attacker.splice(randomTargetIndex, 1);
+    return casualty;
+  }
+
+  showStatus() {
+    if (this.saxonArmy.length === 0) {
+      return 'Vikings have won the war of the century!';
+    } else if (this.vikingArmy.length === 0) {
+      return 'Saxons have fought for their lives and survived another day...';
+    } else if (this.saxonArmy.length >= 1 && this.vikingArmy.length)
+      return 'Vikings and Saxons are still in the thick of battle.';
+  }
+}
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
